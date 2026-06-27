82ccaf54900873741394d9ce1508fe67e3dd2029
Devicharan Prajapati <devicharan2724@gmail.com>
2026-06-27 22:07:36 +0530

    feat: enhance category controller and routes with get and update functionalities

--- Backend/src/controllers/category.controller.js
+++ Backend/src/controllers/category.controller.js
@@ -1,4 +1,4 @@
-const category = require("../models/category.model.js");
+const Category = require("../models/category.model.js");
 const User = require("../models/user.model.js");
 
 const createCategory = async (req, res) => {
@@ -13,7 +13,7 @@ const createCategory = async (req, res) => {
     }
 
 
-    const addCategory = await category.create({
+    const addCategory = await Category.create({
       userId: req.user.id,
       name,
       type,
@@ -31,11 +31,72 @@ const createCategory = async (req, res) => {
     });
   }
 };
-// getCategories();
-// getCategoryById();
-// updateCategory();
-// deleteCategory();
+
+const getCategories = async(req, res)=>{
+try {
+    const categories = await Category.find(
+     { userId : req.user.id}
+    ).select("-password");
+  
+    if(categories.length === 0){ //because find returns array
+      return res.status(404)
+      .json({success : false, message : "Categories are empty or not found!"})
+    }
+  
+    return res
+    .status(200)
+    .json({
+      success: true,
+      message : "Categories Fetched successful",
+      Category : categories
+    })
+} catch (error) {
+  return res.status(500)
+  .json({success :false, message : error.message})
+}
+};
+
+// const getCategoryById = async(req, res)=>{
+
+// };
+
+const updateCategory = async(req, res)=>{
+
+try {
+   const {newName} = req.body;
+   const updatedName = Category.findByIdAndUpdate(
+    req.user.id,
+    {
+      name : newName
+    },
+    {
+      new : true
+    }
+  );
+  
+  return res
+  .status(201)
+  .json({
+    success : true,
+    message : "Category updated successfully!"
+  })
+} catch (error) {
+  return res.status(500)
+  .json({success :false, message : error.message})
+}
+};
+
+const deleteCategory = async(req, res)=>{
+  const deletedCategory = await Category.findByIdAndUpdate(req.user.id);
+
+  return res
+  .status(201)
+  .json({success : true, message : "Category deleted successfully!"})
+};
 
 module.exports = {
   createCategory,
+  getCategories,
+  updateCategory,
+  deleteCategory
 };
--- Backend/src/routes/category.route.js
+++ Backend/src/routes/category.route.js
@@ -1,11 +1,21 @@
-const express = require('express');
-const {createCategory} = require('../controllers/category.controller');
-const verifyToken = require('../middleware/auth.middleware');
+const express = require("express");
+const {
+  createCategory,
+  getCategories,
+  updateCategory,
+  deleteCategory
+} = require("../controllers/category.controller");
 
+const verifyToken = require("../middleware/auth.middleware");
 
 const router = express.Router();
 
 router.post("/addCategory", verifyToken, createCategory);
+router.get("/getCategory", verifyToken, getCategories);
+router.put("/updateCategory", verifyToken, updateCategory);
+router.delete("/deleteCategory", verifyToken, deleteCategory);
+
+
 
 
 module.exports = router;
