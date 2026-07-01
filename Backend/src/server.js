7221295a5d4bff318398c5914c92ae7db80c7004
Devicharan Prajapati <devicharan2724@gmail.com>
2026-07-02 01:24:12 +0530

    refactor: improve CORS configuration for better clarity and structure

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,37 +1,38 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const connectDB = require("./config/db")
-const userRoutes = require("./routes/user.route")
-const categoryRoutes = require('./routes/category.route')
-const transactionRoutes = require("./routes/transaction.route")
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/user.route");
+const categoryRoutes = require("./routes/category.route");
+const transactionRoutes = require("./routes/transaction.route");
 const cors = require("cors");
-
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // local development
+      "https://expense-tracker-eight-zeta-12.vercel.app/", // your Vercel URL
+    ],
+    credentials: true,
+  }),
+);
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
 //Routs
-app.use("/api/users", userRoutes);  
+app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
-app.use("/api/transaction", transactionRoutes)
+app.use("/api/transaction", transactionRoutes);
 
-app.get("/", (req, res)=>{
-  res.send('Hollo world');
+app.get("/", (req, res) => {
+  res.send("Hollo world");
+});
 
-})
-
-
-
-app.listen(port, ()=>{
-    console.log(`app listening on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
