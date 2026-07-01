2164857e3afb9297b0fefdd3a4b148162900e025
Devicharan Prajapati <devicharan2724@gmail.com>
2026-07-01 22:06:38 +0530

    style: update Navbar user avatar background color and text color for improved visibility

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -16,8 +16,8 @@ const Navbar = () => {
       </div>
 
       <div className="flex gap-1 items-center cursor-pointer  hover:bg-olive-200 p-2 rounded-xl">
-        <div className="w-10 h-10 bg-red-400 rounded-full flex justify-center items-center">
-          <h2 className="font-bold text-2xl text-center"> {user?.name[0]}</h2>
+        <div className="w-10 h-10 bg-green-600 rounded-full flex justify-center items-center">
+          <h2 className="font-bold text-2xl text-center text-amber-50"> {user?.name[0]}</h2>
         </div>
         <div>
           <p className="font-semibold">{user?.name}</p>
